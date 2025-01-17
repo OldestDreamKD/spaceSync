d534dce1b203c6e5493dfec1bc44a078d1403f46
Karthik <karthikwrk05@gmail.com>
2025-01-17 20:44:30 +0530

    pacakges updated

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const MapMarkerRoutes = require('./routes/mapMarkerRoutes');
 const BookingDetailsRoutes = require('./routes/bookingDetailsRoutes');
 const AdminRoutes = require('./routes/adminRoutes');
 const path = require('path');
+const fs = require('fs');
+
 
 database();
 
@@ -23,14 +25,44 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // For all other routes, send back the React app's index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  console.log('Serving static files from:', path.join(__dirname, 'client/build'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  console.log('Serving static files from:', (path.join(__dirname, '../client/build/index.html')));
 
 });
+
+function listAllFiles(dirPath, filesList = []) {
+  const files = fs.readdirSync(dirPath); // Synchronous to ensure proper execution in sequence
+
+  files.forEach((file) => {
+    const fullPath = path.join(dirPath, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      // If it's a directory, recursively add its contents
+      listAllFiles(fullPath, filesList);
+    } else {
+      // If it's a file, push it to the list
+      filesList.push(fullPath);
+    }
+  });
+
+  return filesList;
+}
+
+const directoryPath = __dirname; // Start from the current directory
+
+try {
+  const allFiles = listAllFiles(directoryPath);
+  console.log('All files and directories:', allFiles);
+
+  // Optionally write the list to a file for easier viewing
+  fs.writeFileSync(path.join(__dirname, 'allFilesList.log'), allFiles.join('\n'), 'utf8');
+  console.log('File list written to allFilesList.log');
+} catch (err) {
+  console.error('Error reading directory:', err);
+}
 
 app.use("/api/auth", authRoutes);
 app.use('/api/floormaps', FloorMapRoutes);
